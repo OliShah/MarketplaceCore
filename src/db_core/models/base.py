7875abbe44bbd7b8c21439cbf1ace1d59bcbfa3b
OliShah <oli.shah86@gmail.com>
2026-06-02 13:03:02 +0200

    db_core/user models taken out. They belong in consumer side.

--- src/db_core/models/base.py
+++ src/db_core/models/base.py
@@ -1,5 +1,8 @@
-from sqlalchemy.orm import DeclarativeBase
+# Base is the registry + metadata for SQLAlchemy
+# Every db model in the application should inherit from this base class
+#SQLalchemy now knows: “These are all the tables in this application.”
 
+from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
     pass
